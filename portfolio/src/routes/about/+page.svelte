3bf3e295babef9778a0cf8f71bcebdf49a42edf0
Aleksandra Skarzycka <c00294397@setu.ie>
2025-05-02 19:40:00 +0100

    minor about change

--- portfolio/src/routes/about/+page.svelte
+++ portfolio/src/routes/about/+page.svelte
@@ -14,7 +14,7 @@
         <p>My name is Aleksandra Skarzycka. I am 20 years old, born on the 6th of 
             April 2005 in Poland, but living in Ireland since 9 months old. I currently
             live in Carlow. <br> <br>
-            I am studing Interactive Digital Art and Design at SETU Carlow, and I hope to
+            I am studing Interactive Digital Art and Design at SETU Carlow, graduating in 2027, and I hope to
             grow and better my skills. I really hope to become great at art both traditionally
             and digitally so I could become a concept artist for shows/games. I would also really
             like to improve on my coding skills so I can continue to build awesome websites. <br> <br>
